c8a46cbc13ed0b19b691a620d5ab52e92e0a8e68
riyadhossen01 <riyadhossen01work@gmail.com>
2026-05-11 19:43:10 +0600

    lesson button add

---
+++ script/index.js
@@ -0,0 +1,51 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLessons(json.data));
+};
+
+const loadLevelWord = (id) =>{
+    const url =`https://openapi.programming-hero.com/api/level/${id}`
+
+    fetch(url)
+    .then((res) => res.json())
+    .then(data => displayLevelWord(data.data));
+
+};
+const displayLevelWord =(words)=>{
+
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <p></p>
+        `
+
+    });
+
+};
+
+const displayLessons = (lessons) => {
+  console.log(lessons);
+
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+
+    btnDiv.innerHTML = `
+      <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>
+        Lesson - ${lesson.level_no}
+      </button>
+    `;
+
+    levelContainer.appendChild(btnDiv);
+  }
+};
+
+loadLessons();
