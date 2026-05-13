4887f59a7c81834962088c73d5ce645224e6fbe0
riyadhossen01 <riyadhossen01work@gmail.com>
2026-05-13 18:51:08 +0600

    card complete

--- script/index.js
+++ script/index.js
@@ -4,27 +4,39 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
-const loadLevelWord = (id) =>{
-    const url =`https://openapi.programming-hero.com/api/level/${id}`
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`
 
-    fetch(url)
+  fetch(url)
     .then((res) => res.json())
     .then(data => displayLevelWord(data.data));
 
 };
-const displayLevelWord =(words)=>{
+const displayLevelWord = (words) => {
 
-    const wordContainer = document.getElementById("word-container");
-    wordContainer.innerHTML = "";
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
 
-    words.forEach((word) => {
-        console.log(word);
-        const card = document.createElement("div");
-        card.innerHTML = `
-        <p></p>
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = ` 
+      <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl ">${word.word}</h2>
+        <p class="font-semibold">meaning /pronunciation</p>
+        <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+
+        <div class="flex justify-between items-center">
+        <button class="btn bg-[#A191FF10] hover:bg-[#A191FF80]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#A191FF10] hover:bg-[#A191FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+
+
+    </div>
+
         `
-
-    });
+    wordContainer.append(card);
+  });
 
 };
 
